1c23b42d54a648b2f1983dc038fc6fb919fe7ff8
Shivaji Kanse <159415473+shivajikanse@users.noreply.github.com>
2024-07-29 19:03:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
